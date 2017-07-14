aceab6ca0bd6597a5272f62f28619093eeb74985
Daniel Costa <daniel-costa@ufu.br>
2017-07-13 23:38:42 -0300

    Update MethodsHandle.java

--- Servidor/src/Project/MethodsHandle.java
+++ Servidor/src/Project/MethodsHandle.java
@@ -14,6 +14,7 @@ import ThriftGenerate.Vertex;
 import ThriftGenerate.Methods;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
@@ -46,6 +47,8 @@ public class MethodsHandle implements Methods.Iface {
     @Override
     public String createVertex(Vertex v) throws TException {
         DadosVertex result = null;
+        List l = new ArrayList<Integer>();
+
         try {
             result = dbVertex.searchVertex(v.idVertex);
         } catch (IOException e) {
@@ -54,7 +57,7 @@ public class MethodsHandle implements Methods.Iface {
 
         if (result == null) {
             try {
-                dbVertex.insertVertex(new DadosVertex(v.nameVertex, v.idVertex, v.color, v.description, v.weight, 1));
+                dbVertex.insertVertex(new DadosVertex(v.nameVertex, v.idVertex, v.color, v.description, v.weight, 1, l));
                 return "Dados inseridos com sucesso";
             } catch (IOException e) {
                 return "Erro na inserção"
@@ -96,33 +99,22 @@ public class MethodsHandle implements Methods.Iface {
         System.out.println("Removendo dados...");
         DadosVertex result = null;
 
-        DadosEdge e1 = null;
-        DadosEdge e2 = null;
-
         try {
             result = dbVertex.searchVertex(idVertex);
 
-            e1 = dbEdge.searchEdge(result.getIdEdge1());
-            e2 = dbEdge.searchEdge(result.getIdEdge2());
-
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         if (result == null) {
             return "Registro inexistente";
         } else {
             try {
-                if (e1 != null) {
-                    dbEdge.removeEdge(e1.getIdEdge());
+                for (int i = 0; i < result.getEd().size(); i++) {
+                    deleteEdge(result.getEd().get(i));
                 }
-                if (e2 != null) {
-                    dbEdge.removeEdge(e2.getIdEdge());
-                }
-                
                 dbVertex.removeVertex(idVertex);
+
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             return "Registro removido!";
@@ -220,6 +212,7 @@ public class MethodsHandle implements Methods.Iface {
 
         try {
             result = dbEdge.searchEdge(a.idEdge);
+
             v1 = dbVertex.searchVertex(a.idVertex1);
             v2 = dbVertex.searchVertex(a.idVertex2);
 
@@ -232,10 +225,10 @@ public class MethodsHandle implements Methods.Iface {
             if (v1 == null || v2 == null) {
                 return "Não é possivel adicionar, algum dos vértice inexistentes";
             } else {
-                try {
-                    v1.setIdEdge2(a.idEdge);
-                    v2.setIdEdge1(a.idEdge);
+                v1.setEd(a.idEdge);
+                v2.setEd(a.idEdge);
 
+                try {
                     dbEdge.insertEdge(new DadosEdge(a.nameEdge, a.idEdge, a.weight, a.description, a.flagDirected, a.idVertex1, a.idVertex2, 1));
                     return "Aresta " + a.nameEdge + " Inserida";
 
@@ -263,7 +256,7 @@ public class MethodsHandle implements Methods.Iface {
         String a = readNameVertex(result.getIdVertex1());
         String b = readNameVertex(result.getIdVertex2());
 
-        return "Aresta '" + result.getNameEdge() + "' possui os vértices: (" + a + "-----" + b + ")";
+        return "Vértices: [" + a + " - " + b + "]";
     }
 
     @Override
@@ -275,34 +268,54 @@ public class MethodsHandle implements Methods.Iface {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        String textFinal = null;
+        DadosEdge e1 = null;
 
-        String a = readNameEdge(result.getIdEdge1());
-        String b = readNameEdge(result.getIdEdge2());
+        for (int i = 0; i < result.getEd().size(); i++) {
+            try {
+                e1 = dbEdge.searchEdge(i);
 
-        return "Vértice '" + result.getNameVertex() + "' possui as arestas: (" + a + "-----" + b + ")";
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            textFinal = textFinal + e1.getNameEdge() + " - ";
+        }
+        return "Arestas: [" + textFinal + "]";
     }
 
     @Override
     public String listVertexForVertex(int idVertex) throws TException {
 
         DadosVertex result = null;
+        DadosEdge ed = null;
+        DadosEdge ed2 = null;
 
+        DadosVertex v1 = null;
+        DadosVertex v2 = null;
+        String textFinal = null;
         try {
             result = dbVertex.searchVertex(idVertex);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            for (int i = 0; i < result.getEd().size(); i++) {
+                ed = dbEdge.searchEdge(i);
+                v1 = dbVertex.searchVertex(ed.getIdVertex1());
+
+                textFinal = textFinal + v1.getNameVertex() + " - ";
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(MethodsHandle.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        String a = readNameEdge(result.getIdEdge1());
-        String b = readNameEdge(result.getIdEdge2());
-
-        return "Arestas: " + a + "-----" + b;
+        return "Vértices:[" + textFinal + "]";
     }
 
     @Override
     public String updateVertex(int idVertex, String color, String description, double peso) throws TException {
         DadosVertex result = null;
+        List l = new ArrayList<Integer>();
+
         try {
             result = dbVertex.searchVertex(idVertex);
 
@@ -315,7 +328,7 @@ public class MethodsHandle implements Methods.Iface {
             return "Vertice não encontrada";
         } else {
             try {
-                dbVertex.insertVertex(new DadosVertex(result.getNameVertex(), result.getIdVertex(), color, description, peso, result.getVersion() + 1));
+                dbVertex.insertVertex(new DadosVertex(result.getNameVertex(), result.getIdVertex(), color, description, peso, result.getVersion() + 1, l));
                 return "Vertice " + result.getNameVertex() + " atualizado";
 
             } catch (IOException ex) {
@@ -348,5 +361,139 @@ public class MethodsHandle implements Methods.Iface {
                         + "Erro: " + ex.getMessage();
             }
         }
+    }
+
+    // Atributos usados na funcao encontrarMenorCaminho
+    // Lista que guarda os vertices pertencentes ao menor caminho encontrado
+    List<DadosVertex> menorCaminho = new ArrayList<DadosVertex>();
+
+    // Variavel que recebe os vertices pertencentes ao menor caminho
+    DadosVertex verticeCaminho;
+
+    // Variavel que guarda o vertice que esta sendo visitado
+    Vertex VerticeAtual = new Vertex();
+
+    // Variavel que marca o vizinho do vertice atualmente visitado
+    //Vertex vizinho = new Vertex();
+    DadosVertex verticeVizinho;
+
+    // Lista dos vertices que ainda nao foram visitados
+    List<DadosVertex> naoVisitados = new ArrayList<DadosVertex>();
+
+    // Algoritmo de Dijkstra
+    @Override
+    public List<String> dijkstra(Vertex v1, Vertex v2) throws TException {
+
+        int idEdgeTemp;
+        DadosEdge cadaEdge = null;
+        DadosVertex verticeDestino = null;
+        DadosVertex vAtual = null;
+
+        DadosVertex vertice1 = new DadosVertex(v1.nameVertex, v1.idVertex, v1.color, v1.description, v1.weight, v1.version, v1.ed);
+        menorCaminho.add(vertice1);
+
+        try {
+            for (int i = 0; i < dbVertex.ShowDataVertex().size(); i++) {
+                DadosVertex result = null;
+
+                String a = dbVertex.ShowDataVertex().get(i);
+                int id = Integer.parseInt(a);
+
+                result = dbVertex.searchVertex(id);
+
+                if (result.getIdVertex() == (v1.getIdVertex())) {
+
+                    result.setDistance(0);
+
+                } else {
+
+                    result.setDistance(999);
+                }
+                // Insere o vertice na lista de vertices nao visitados
+                //String nVisitado = Integer.toString(result.getIdVertex());
+
+                naoVisitados.add(result);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MethodsHandle.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Collections.sort(naoVisitados);
+        for (int i = 0; i < naoVisitados.size(); i++) {
+            System.out.println(naoVisitados.get(i).getNameVertex());
+        }
+        // O algoritmo continua ate que todos os vertices sejam visitados
+        while (!naoVisitados.isEmpty()) {
+
+            // Toma-se sempre o vertice com menor distancia, que eh o primeiro da lista
+            vAtual = naoVisitados.get(0);
+            System.out.println("Pegou esse vertice:  " + vAtual.getNameVertex());
+
+            //		 * Para cada vizinho (cada aresta), calcula-se a sua possivel
+            //		 * distancia, somando a distancia do vertice atual com a da aresta
+            //		 * correspondente. Se essa distancia for menor que a distancia do
+            //		 * vizinho, esta eh atualizada.
+            for (int i = 0; i < vAtual.getEd().size(); i++) {
+
+                idEdgeTemp = vAtual.getEd().get(i);
+
+                try {
+                    cadaEdge = dbEdge.searchEdge(idEdgeTemp);
+                    verticeVizinho = dbVertex.searchVertex(cadaEdge.getIdEdge());
+                } catch (IOException ex) {
+                    Logger.getLogger(MethodsHandle.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                System.out.println("Olhando a aresta vizinha de " + vAtual.getNameVertex() + ": "
+                        + verticeVizinho.getNameVertex());
+
+                if (!verticeVizinho.verificarVisita()) {
+
+                    // Comparando a distância do vizinho com a possível
+                    // distância
+                    if (verticeVizinho.getDistance() > (vAtual.getDistance() + cadaEdge.getWeight())) {
+                        verticeVizinho.setDistance(vAtual.getDistance()
+                                + cadaEdge.getWeight());
+                        verticeVizinho.setPai(vAtual);
+
+                        //			 * Se o vizinho eh o vertice procurado, e foi feita uma
+                        //			 * mudanca na distancia, a lista com o menor caminho
+                        //			 * anterior eh apagada, pois existe um caminho menor
+                        //			 * vertices pais, ateh o vertice origem.
+                        try {
+                            verticeDestino = dbVertex.searchVertex(v2.idVertex);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+
+                        if (verticeVizinho == verticeDestino) {
+                            menorCaminho.clear();
+                            verticeCaminho = verticeVizinho;
+                            menorCaminho.add(verticeVizinho);
+
+                            while (verticeCaminho.getPai() != null) {
+
+                                menorCaminho.add(verticeCaminho.getPai());
+                                verticeCaminho = verticeCaminho.getPai();
+                            }
+                        }
+                    }
+                }
+            }
+            // Marca o vertice atual como visitado e o retira da lista de nao
+            // visitados
+            vAtual.Visitado();
+            naoVisitados.remove(vAtual);
+            
+            System.out.println("Nao foram visitados ainda:" + naoVisitados);
+        }
+
+        List<String> s = new ArrayList<String>();
+        
+        for (int i = 0; i < menorCaminho.size(); i++) {
+            s.add(menorCaminho.get(i).getNameVertex());
+            //System.out.println("Menor caminho ->" + menorCaminho.get(i).getNameVertex());
+        }
+        return s;
     }
 }
